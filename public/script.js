21f71a857f5b2a230f03f7e436c7ef0bb820e0a2
Leonard Tan <tetratorus@gmail.com>
2020-12-02 12:45:07 +0800

    a

--- public/script.js
+++ public/script.js
@@ -65,7 +65,7 @@ function toArrayBuffer(buf) {
   //     try {
   //         document.getElementById("text").textContent = algId.toString()
   // console.log(algId + " passed")
-  const requestedBytes = 1024; // 10MB
+  const requestedBytes = 1024 * 1024; // 10MB
   async function requestQuota() {
     return new Promise((resolve, reject) => {
       navigator.webkitPersistentStorage.requestQuota(requestedBytes, resolve, reject);
